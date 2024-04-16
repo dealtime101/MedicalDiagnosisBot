6c6cbc51ff31da41d4ee791719cdd1f9cd6e728d
ubidan <dany.clairvoyant@ubisoft.com>
2024-04-16 11:26:11 -0400

    Project Implementation - Part 2
    Program Entry Point
    
    Entry point and simply provides a prompt to the user, handles their selection, and directs them to the next part of the program.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-#print("Welcome doctor!", "What would you like to do today?")
+welcome_prompt = ("Welcome doctor, what would you like to do today?\n - To list all patients, press 1\n "
+                  "- To run a new diagnosis, press 2\n - To Quit, press q\n")
 
-name = input("What is your name?\n")
-print(name)
+
+def list_patients():
+    print("Listing patients and diagnoses")
+
+
+def start_new_diagnosis():
+    print("Starting a new diagnosis")
+
+
+def main():
+    doctor_selection = input(welcome_prompt)
+    if doctor_selection == "1":
+        list_patients()
+    elif doctor_selection == "2":
+        start_new_diagnosis()
+    elif doctor_selection == "q":
+        return
+
+
+main()
