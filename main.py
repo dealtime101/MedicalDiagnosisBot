1c5e411a84565862258c17639d04d02e572a995b
dealtime101 <dealtime101@hotmail.com>
2024-04-16 21:17:11 -0400

    Project Implementation – Part 4
    Inquire the user about the eye appearance of the patient and output a diagnosis.
    
    Implement the assess_eyes function and output a “No dehydration” or “Severe dehydration” result based on the user’s selection.

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ welcome_prompt = ("Welcome doctor, what would you like to do today?\n "
                   "- To run a new diagnosis, press 2\n "
                   "- To Quit, press q\n")
 
-
 name_prompt = "What is the patient's name?\n"
 appearance_prompt = ("How is the patient's general appearance?\n"
                      "- 1: Normal appearance\n"
                      "- 2: Irritable or lethargic\n")
+eye_prompt = ("How are the patient's eyes?\n"
+              "- 1: Eyes normal or slightly sunken\n"
+              "- 2: Eyes very sunken\n")
+severe_dehydration = "Severe dehydration"
+some_dehydration = "Some dehydration"
+no_dehydration = "No dehydration"
 
 
 def list_patients():
@@ -19,17 +24,25 @@ def assess_skin():
 
 
 def assess_eyes():
-    print("Assessing eyes")
+    eyes = input(eye_prompt)
+    if eyes == "1":
+        return no_dehydration
+    elif eyes == "2":
+        return severe_dehydration
+
+
+def assess_appearance():
+    appearance = input(appearance_prompt)
+    if appearance == "1":
+        diagnosis = assess_eyes()
+    elif appearance == "2":
+        assess_skin()
 
 
 def start_new_diagnosis():
     print("Starting a new diagnosis")
     name = input(name_prompt)
-    appearance = input(appearance_prompt)
-    if appearance == "1":
-        assess_eyes()
-    elif appearance == "2":
-        assess_skin()
+    assess_appearance()
 
 
 def main():
